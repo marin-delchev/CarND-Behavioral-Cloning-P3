bf223b9ad4df50d23a7f3f78e6ff0f124b242ccc
marin-delchev <MarinDelchev@protonmail.com>
2017-04-13 11:08:33 +0300

    Reduce color channels

--- model_n.py
+++ model_n.py
@@ -10,7 +10,7 @@ from keras.layers import Flatten, Convolution2D, MaxPooling2D, Dense, Lambda, Cr
 from keras.optimizers import Adam
 
 lines = []
-with open('./data/driving_log.csv') as csv_file:
+with open('./data/data/driving_log.csv') as csv_file:
     reader = csv.reader(csv_file)
     for line in reader:
         lines.append(line)
@@ -40,7 +40,7 @@ def generator(samples, batch_size=64):
                 for i in range(3):
                     source_path = batch_sample[i]
                     filename = source_path.split('/')[-1]
-                    current_path = './data/IMG/' + filename
+                    current_path = './data/data/IMG/' + filename
                     image = cv2.imread(current_path)
                     images.append(image)
                     images.append(cv2.flip(image, 1))
@@ -63,10 +63,10 @@ validation_generator = generator(validation_samples)
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(1, 1, 1,  subsample=(1, 1), border_mode='same', init='glorot_uniform'))
 model.add(Convolution2D(16, 5, 5, subsample=(2, 2), border_mode='valid', activation='elu'))
 model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid', activation='elu'))
 model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode='valid', activation='elu'))
-model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dropout(0.1))
 model.add(Dense(256, activation='elu'))
@@ -78,9 +78,9 @@ adam = Adam(lr=0.0001)
 model.compile(loss='mse', optimizer=adam)
 model.summary()
 model.fit_generator(train_generator,
-                    samples_per_epoch=len(train_samples)*3,
+                    samples_per_epoch=len(train_samples)*6,
                     validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples)*3,
+                    nb_val_samples=len(validation_samples)*6,
                     nb_epoch=5,
                     verbose=1)
 model.save('model.h5')
